fa3a14833b0ab3ffc40763c00eda3d0c39f27034
DemonDonuts <safeliz928@gmail.com>
2026-04-15 18:30:50 -0400

    fixed canvas - no error in console

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
-var canvas = document.getElementById('gameCanvas');
-var context = canvas.getContext('2d');
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+// paddle
+var player1 = new GameObject();
+player1.x = 20;
+player1.y = canvas.height / 2 - 50;
+player1.width = 20;
+player1.height = 100;
+player1.color = "white";
+
+setInterval(animate, 1000 / 60);
 
 function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    player1.drawRect(context);
 }
-
-setInterval(animate, 1000 / 60);
