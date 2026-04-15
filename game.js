831fc88ff4dae15a66a7b3de992b434c9cdffec3
DemonDonuts <safeliz928@gmail.com>
2026-04-15 18:43:45 -0400

    fixed everything.

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-
-// paddle
 var player1 = new GameObject();
+
+console.log(document.getElementById("canvas"));
+
 player1.x = 20;
 player1.y = canvas.height / 2 - 50;
 player1.width = 20;
 player1.height = 100;
-player1.color = "white";
+player1.color = "blue";
 
 setInterval(animate, 1000 / 60);
 
